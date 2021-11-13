4cfe2c73a246864e35a2a8422282a854aeb6ff0c
Chris <chris@pinecone.us>
2021-11-13 09:32:44 -0500

    Add pointer

--- src/content/Pointers/index.js
+++ src/content/Pointers/index.js
@@ -37,6 +37,7 @@ export class Pointers extends React.Component<Props> {
     renderRules() {
         return (
             <div style={{ marginTop: 40, paddingLeft: 50, paddingRight: 50 }}>
+                <p style={{ marginTop: 10, marginBottom: 20 }}>- The game will end on December 1st.</p>
                 <p style={{ marginTop: 10, marginBottom: 20 }}>- I do not have any allergies, and I am not on any medications.</p>
                 <p style={{ marginTop: 10, marginBottom: 20 }}>- I wear contacts and glasses. You can choose to not let me wear or service them, but that may result in my vision being impared and thus my ability to complete tasks being diminished.</p>
                 <p style={{ marginTop: 10, marginBottom: 20 }}>- I live about a 30-40 minute drive from the center of D.C. Be aware of this when playing, it may be boring to watch me sit in a car if you vote for me to visit there.</p>
